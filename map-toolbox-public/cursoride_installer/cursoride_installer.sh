6bb7ae282007bb02ebafd25f5b1dbc6e098395f8
MauroPereira <mauro.a.pereira@gmail.com>
2025-05-18 20:28:58 -0300

    link updated #done

--- map-toolbox-public/cursoride_installer/cursoride_installer.sh
+++ map-toolbox-public/cursoride_installer/cursoride_installer.sh
@@ -33,7 +33,7 @@ APPIMAGE_FILE="/tmp/Cursor.AppImage"
 INSTALL_PATH="/opt/cursor"
 WRAPPER_PATH="/usr/bin/cursor"
 DESKTOP_FILE="/usr/share/applications/cursor.desktop"
-DOWNLOAD_URL="https://downloads.cursor.com/production/8ea935e79a50a02da912a034bbeda84a6d3d355d/linux/x64/Cursor-0.50.4-x86_64.AppImage"
+DOWNLOAD_URL="https://downloads.cursor.com/production/96e5b01ca25f8fbd4c4c10bc69b15f6228c80771/linux/x64/Cursor-0.50.5-x86_64.AppImage"
 
 # Check for required dependencies
 echo "🔍 Checking dependencies..."
